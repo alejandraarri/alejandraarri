5b88ff5808171bb642781acd7c2337af59750f55
alejandraarri <aleja.arri@gmail.com>
2017-08-01 20:33:06 +0200

    Contact form improved

--- src/js/main.js
+++ src/js/main.js
@@ -25,13 +25,42 @@ $(function() {
 });
 
 $(document).ready(function(){
-     $(window).bind('scroll', function() {
-     var navHeight = $( window ).height();
-           if ($(window).scrollTop() > navHeight) {
-               $('nav').addClass('navbar--fixed');
-           }
-           else {
-               $('nav').removeClass('navbar--fixed');
-           }
+    $(window).bind('scroll', function() {
+        var navHeight = $( window ).height();
+        if ($(window).scrollTop() > navHeight) {
+            $('nav').addClass('navbar--fixed');
+        } else {
+            $('nav').removeClass('navbar--fixed');
+        }
+    });
+
+    $('#back-button').click(function(e){
+        $("#contact__feedback").hide();
+        $("#back-button").hide();
+        $("#contact__main-form").show();
+    });
+
+    $('#send-button').click(function(e){
+      $.ajax({
+          data: {"name" : $("#name").val(), "email" : $("#email").val(), "message": $("#message").val()},
+          type: "POST",
+          dataType: "json",
+          url: "php/contact-form-handler.php"
+      })
+      .done(function( data, textStatus, jqXHR ) {
+          $("#contact__main-form").hide();
+          $("#contact__feedback").show();
+          if (data && data.length != 0 ){
+              $("#final-message").html(data[0].statusText);
+              if (data[0].status != "00" ){
+                  $("#back-button").show();
+              }
+          }
+      })
+      .fail(function( jqXHR, textStatus, errorThrown ) {
+          if ( console && console.log ) {
+              console.error( "La solicitud a fallado: " +  textStatus);
+          }
       });
-  });
+    });
+});
