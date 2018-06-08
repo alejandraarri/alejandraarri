12754c224890c2d1b06fca71913db0207d1837ca
alejandraarri <>
2018-06-08 20:55:28 +0200

    Package.json and gulpfile.js clean up. .gitignore added.

--- gulpfile.js
+++ gulpfile.js
@@ -1,134 +1,128 @@
 var gulp        = require('gulp'),
-    clean       = require('gulp-clean'),
-    concat      = require('gulp-concat'),
-    postcss     = require('gulp-postcss'),
-    cssnext     = require('postcss-cssnext'),
-    precss      = require('precss'),
-    notify      = require('gulp-notify'),
-    runSequence = require('run-sequence');
+clean       = require('gulp-clean'),
+concat      = require('gulp-concat'),
+postcss     = require('gulp-postcss'),
+cssnext     = require('postcss-cssnext'),
+plumber     = require('gulp-plumber'),
+precss      = require('precss'),
+nano        = require('gulp-cssnano'),
+notify      = require('gulp-notify'),
+uglify      = require('gulp-uglify'),
+runSequence = require('run-sequence');
 
 var browserSync = require('browser-sync').create();
 
-  /*  precss = require('precss'),*/
-  /*  plumber = require('gulp-plumber'),*/
-    /*nano = require('gulp-cssnano'),*/
-    //livereload = require('gulp-livereload'),
-
 var dist_dir    = './dist/',
-    src_dir     = './src/'
-    css_src     = src_dir  + 'css/main.css',
-    css_dist    = dist_dir + 'css/',
-    js_src      = src_dir  + 'js/main.js',
-    js_dist     = dist_dir + 'js/',
-    fonts_src   = src_dir  + 'fonts/*.*',
-    fonts_dist  = dist_dir + 'fonts/',
-    images_src  = src_dir  + 'images/*.*';
-    images_dist = dist_dir + 'images/';
+src_dir     = './src/'
+css_src     = src_dir  + 'css/main.css',
+css_dist    = dist_dir + 'css/',
+js_src      = src_dir  + 'js/main.js',
+js_dist     = dist_dir + 'js/',
+fonts_src   = src_dir  + 'fonts/*.*',
+fonts_dist  = dist_dir + 'fonts/',
+images_src  = src_dir  + 'images/*.*';
+images_dist = dist_dir + 'images/';
 
 // Clean
 gulp.task('clean', function() {
   return gulp.src(['./dist/*'], {read: false})
-    .pipe(clean());
+  .pipe(clean());
 });
 
 // Create base dist
 gulp.task('copyfonts', function() {
-   gulp.src(fonts_src)
-   .pipe(gulp.dest(fonts_dist));
+  gulp.src(fonts_src)
+  .pipe(gulp.dest(fonts_dist));
 });
 gulp.task('copyimages', function() {
-   gulp.src(images_src)
-   .pipe(gulp.dest(images_dist));
+  gulp.src(images_src)
+  .pipe(gulp.dest(images_dist));
 });
 gulp.task('copyindex', function() {
-    gulp.src('./src/index.html')
-    // Perform minification tasks, etc here
-    .pipe(gulp.dest(dist_dir));
+  gulp.src('./src/index.html')
+  // Perform minification tasks, etc here
+  .pipe(gulp.dest(dist_dir));
 });
 
 //css
 gulp.task('css', function() {
-    var processors = [
-        cssnext,
-        precss
-    ];
-    return gulp.src(css_src)
-        .pipe(postcss(processors))
-        .pipe(gulp.dest(css_dist))
-        .pipe(notify({ message: 'css task complete' }));
+  var processors = [
+    cssnext,
+    precss
+  ];
+  return gulp.src(css_src)
+  .pipe(postcss(processors))
+  .pipe(gulp.dest(css_dist))
+  .pipe(notify({ message: 'css task complete' }));
 });
 
 //css
+//TO-DO: Use environment variables to run production tasks
 gulp.task('css-pro', function() {
-    var processors = [
-        cssnext,
-        precss
-    ];
-  /*  var configNano = {
-      autoprefixer: { browsers: 'last 2 versions' },
-      discardComments: { removeAll: true },
-      safe: true
-    };*/
-    return gulp.src(css_src)
-        .pipe(postcss(processors))
-        .pipe(gulp.dest(css_dist))
-      //  .pipe(plumber())
-        //.pipe(nano(configNano))
-      //  .pipe(gulp.dest(dist_dir));
-      //  .pipe( browserSync.stream() )
-      //  .pipe(notify({ message: 'Your CSS is ready ;)' }));
-      .pipe(notify({ message: 'css task complete' }));
+  var processors = [
+    cssnext,
+    precss
+  ];
+  var configNano = {
+    autoprefixer: { browsers: 'last 2 versions' },
+    discardComments: { removeAll: true },
+    safe: true
+  };
+  return gulp.src(css_src)
+  .pipe(postcss(processors))
+  .pipe(gulp.dest(css_dist))
+  .pipe(plumber())
+  .pipe(nano(configNano))
+  .pipe(gulp.dest(css_dist))
+  .pipe( browserSync.stream() )
+  .pipe(notify({ message: 'css task complete' }));
 });
 
 // Scripts
 gulp.task('js', function() {
   return gulp.src(js_src)
-    .pipe(concat('main.js'))
-    .pipe(gulp.dest(js_dist))
-    .pipe(notify({ message: 'Scripts task complete' }));
+  .pipe(concat('main.js'))
+  .pipe(gulp.dest(js_dist))
+  .pipe(notify({ message: 'Scripts task complete' }));
 });
 
 // Scripts
+//TO-DO: Use environment variables to run production tasks
 gulp.task('js-pro', function() {
-  return gulp.src('src/scripts/**/*.js')
-    .pipe(jshint('.jshintrc'))
-    .pipe(jshint.reporter('default'))
-    .pipe(concat('main.js'))
-    .pipe(gulp.dest('dist/scripts'))
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(uglify())
-    .pipe(livereload(server))
-    .pipe(gulp.dest('dist/scripts'))
-    .pipe(notify({ message: 'Scripts task complete' }));
+  return gulp.src(js_src)
+  .pipe(concat('main.js'))
+  .pipe(uglify())
+  .pipe(gulp.dest(js_dist))
+  .pipe(notify({ message: 'Scripts task complete' }));
 });
 
 // create a task that ensures the `css` task is complete before
 // reloading browsers
 gulp.task('css-watch', ['css'], function (done) {
-    browserSync.reload();
-    done();
+  browserSync.reload();
+  done();
 });
 
 // Static server
 gulp.task('serve', ['css'], function() {
 
-    // Serve files from the root of this project
-    browserSync.init({
-        server: {
-            baseDir: dist_dir
-        },
-        browser: "google chrome"
-    });
+  // Serve files from the root of this project
+  browserSync.init({
+    server: {
+      baseDir: dist_dir
+    },
+    browser: "google chrome"
+  });
 
-    // add browserSync.reload to the tasks array to make
-    // all browsers reload after tasks are complete.
-    gulp.watch("./css/*.css", ['css-watch']);
+  // add browserSync.reload to the tasks array to make
+  // all browsers reload after tasks are complete.
+  gulp.watch("./css/*.css", ['css-watch']);
 });
 
 // Watch
 gulp.task('watch', function() {
-    // Watch .css files
-    gulp.watch('src/**/*.css', ['css']);
+  // Watch .css files
+  gulp.watch('src/**/*.css', ['css']);
 
 });
 
@@ -147,5 +141,5 @@ gulp.task('default', function() {
 });
 
 gulp.task('build-pro', function() {
-  runSequence(['css', 'server', 'watch']);
+  runSequence(['copy', 'css-pro', 'js-pro']);
 });
